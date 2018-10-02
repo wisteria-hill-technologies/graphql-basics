1999efacb4755ae14e6cda93b0cf108a6e689b4f
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-10-02 23:58:06 +0100

    adds graphQLServer

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga'
+
+// Type definition (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+  }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query!';
+    },
+    name() {
+      return 'Noby Fujioka';
+    }
+
+  }
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('The server is up!');
+});
