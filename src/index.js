f8bca4e00d43aa562d1cbe4e8e66abc2600b7779
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-10-04 22:21:04 +0100

    adds relationship between author and user

--- src/index.js
+++ src/index.js
@@ -18,11 +18,42 @@ const users = [
   },
   {
     id: '3',
+    name: 'Bill',
+    email: 'bill@emailemail.com',
+    age: 45
+  },
+  {
+    id: '4',
     name: 'Sarah',
     email: 'sarah@emailemail.com',
     age: 27
   },
 ];
+
+const posts = [
+  {
+    id: '001',
+    title: 'My first post',
+    body: 'This is my first post.',
+    published: true,
+    author: '4'
+  },
+  {
+    id: '002',
+    title: 'How to make a pancake',
+    body: 'This is how to do it.',
+    published: false,
+    author: '2'
+  },
+  {
+    id: '003',
+    title: 'Javascript is great!',
+    body: 'It is interesting to learn javascript...',
+    published: true,
+    author: '2'
+  },
+];
+
 
 // Type definition (schema)
 const typeDefs = `
@@ -30,6 +61,7 @@ const typeDefs = `
     users(query: String): [User!]!
     me: User!
     post: Post!
+    posts(query: String): [Post!]!
   }
   
   type User {
@@ -37,6 +69,7 @@ const typeDefs = `
     name: String!
     email: String!
     age: Int
+    posts: [Post!]!
   }
   
   type Post {
@@ -44,6 +77,7 @@ const typeDefs = `
     title: String!
     body: String!
     published: Boolean!
+    author: User!
   }
 `;
 
@@ -51,8 +85,23 @@ const typeDefs = `
 const resolvers = {
   Query: {
     users (parent, args, ctx, info) {
-      return users;
-  },
+      if(!args.query) {
+        return users;
+      }
+      return users.filter(user => {
+        return user.name.toLowerCase().includes(args.query.toLowerCase());
+      });
+    },
+    posts(parent, args, ctx, info) {
+      if(!args.query) {
+        return posts;
+      }
+      return posts.filter(post => {
+        const titleIncludesQuery = post.title.toLowerCase().includes(args.query.toLowerCase());
+        const bodyIncludesQuery = post.body.toLowerCase().includes(args.query.toLowerCase());
+        return titleIncludesQuery || bodyIncludesQuery;
+      });
+    },
     me() {
       return {
         id: '123abc',
@@ -68,6 +117,11 @@ const resolvers = {
         body: 'Hello, World!!!',
         published: true
       }
+    }
+  },
+  Post: {
+    author(parent, args, ctx, info) {
+      return users.find(user => user.id === parent.author);
     }
   }
 };
