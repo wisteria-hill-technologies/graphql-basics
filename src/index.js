7fc9b8531398dd254c9e53f0ec94fe94d95dc004
nobyfujioka <noby.fujioka@volocommerce.com>
2018-10-05 17:51:15 +0100

    adds createComment

--- src/index.js
+++ src/index.js
@@ -95,6 +95,8 @@ const typeDefs = `
   
   type Mutation {
     createUser(name: String!, email: String!, age: Int): User!
+    createPost(title: String!, body: String!, published: Boolean!, author: ID!): Post!
+    createComment(text: String!, author: ID!, post: ID!): Comment!
   }
   
   type User {
@@ -184,6 +186,42 @@ const resolvers = {
       users.push(user);
 
       return user;
+    },
+    createPost(parent, args, ctx, info) {
+
+      const userExists = users.some(user => user.id === args.author);
+      if(!userExists) {
+        throw Error('User not found.');
+      }
+      const post = {
+        id: uuidv4(),
+        title: args.title,
+        body: args.body,
+        published: args.published,
+        author: args.author
+      };
+
+      posts.push(post);
+
+      return post;
+    },
+    createComment(parent, args, ctx, info) {
+      const postExists = posts.some(post => (post.id === args.post) && post.published);
+      const userExists = users.some(user => user.id === args.author);
+      if(!postExists) {
+        throw Error('Post not found or published');
+      }
+      if(!userExists) {
+        throw Error('User not found');
+      }
+      const comment = {
+        id: uuidv4(),
+        text: args.text,
+        author: args.author,
+        post: args.post
+      }
+      comments.push(comment);
+      return comment;
     }
   },
   Post: {
