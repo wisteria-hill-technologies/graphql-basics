13eb6470d2443ab6aab5fbf78e02761de95e0f08
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-10-07 22:10:02 +0100

    delete post subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -56,7 +56,7 @@ const Mutation = {
     }
     return user;
   },
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some(user => user.id === args.data.author);
     if(!userExists) {
       throw Error('User not found.');
@@ -67,18 +67,34 @@ const Mutation = {
     };
 
     db.posts.push(post);
-
+    if (args.data.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'CREATED',
+          data: post
+        }
+      });
+    }
     return post;
   },
-  deletePost(parent, args, { db }, info) {
+  deletePost(parent, args, { db, pubsub }, info) {
     const postIndex = db.posts.findIndex(post => post.id === args.id);
 
     if(postIndex === -1) {
       throw new Error('Post not found.');
     }
-    const deletedPosts = db.posts.splice(postIndex, 1);
+    const [deletedPost] = db.posts.splice(postIndex, 1);
     db.comments = db.comments.filter(comment => comment.post !== args.id);
-    return deletedPosts[0];
+    if (deletedPost.published) {
+      pubsub.publish('post', {
+        post: {
+          mutation: 'DELETED',
+          data: deletedPost
+        }
+      });
+    }
+
+    return deletedPost;
   },
   updatePost(parent, args, { db }, info) {
     const { id, data } = args;
@@ -99,7 +115,7 @@ const Mutation = {
 
     return post;
   },
-  createComment(parent, args, { db }, info) {
+  createComment(parent, args, { db, pubsub }, info) {
     const postExists = db.posts.some(post => (post.id === args.data.post) && post.published);
     const userExists = db.users.some(user => user.id === args.data.author);
     if(!postExists) {
@@ -113,6 +129,7 @@ const Mutation = {
       ...args.data
     }
     db.comments.push(comment);
+    pubsub.publish(`comment ${args.data.post}`, { comment });
     return comment;
   },
   deleteComment(parent, args, { db }, info) {
