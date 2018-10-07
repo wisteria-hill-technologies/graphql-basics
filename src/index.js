0d16819b6973aade04b62f27a5b27aca743fddfe
Noby Fujioka <n.fujioka@abacusmaths.info>
2018-10-07 17:53:22 +0100

    adds update user post comment

--- src/index.js
+++ src/index.js
@@ -1,159 +1,19 @@
 import { GraphQLServer } from 'graphql-yoga';
-import uuidv4 from 'uuid/v4';
 import db from './db';
+import Query from './resolvers/Query';
+import Mutation from './resolvers/Mutation';
+import User from './resolvers/User';
+import Post from './resolvers/Post';
+import Comment from './resolvers/Comment';
+
 
 // Resolvers
 const resolvers = {
-  Query: {
-    users (parent, args, { db }, info) {
-      if(!args.query) {
-        return db.users;
-      }
-      return db.users.filter(user => {
-        return user.name.toLowerCase().includes(args.query.toLowerCase());
-      });
-    },
-    posts(parent, args, { db }, info) {
-      if(!args.query) {
-        return db.posts;
-      }
-      return db.posts.filter(post => {
-        const titleIncludesQuery = post.title.toLowerCase().includes(args.query.toLowerCase());
-        const bodyIncludesQuery = post.body.toLowerCase().includes(args.query.toLowerCase());
-        return titleIncludesQuery || bodyIncludesQuery;
-      });
-    },
-    comments(parent, args, { db }, info) {
-      if(!args.query) {
-        return db.comments;
-      }
-      return db.comments.filter(comment => comment.text.includes(args.query));
-    },
-    me() {
-      return {
-        id: '123abc',
-        name: 'Mike',
-        email: 'mike@myemail.com',
-        age: 35
-      }
-    },
-    post() {
-      return {
-        id: '092',
-        title: 'GraphQL 101',
-        body: 'Hello, World!!!',
-        published: true
-      }
-    },
-  },
-  Mutation: {
-    createUser(parent, args, { db }, info) {
-      const emailTaken = db.users.some((user) => user.email === args.data.email);
-      if(emailTaken) {
-        throw new Error('Email taken.');
-      }
-
-      const user = {
-        id: uuidv4(),
-        ...args.data
-      };
-
-      db.users.push(user);
-
-      return user;
-    },
-    deleteUser(parent, args, { db }, info) {
-      const userIndex = db.users.findIndex(user => user.id === args.id);
-      if (userIndex === -1) {
-        throw new Error('User not found');
-      }
-
-      const deletedUsers = db.users.splice(userIndex, 1);
-      db.posts = db.posts.filter(post => {
-        const match = post.author === args.id;
-        if(match) {
-          db.comments = db.comments.filter(comment => comment.post !== post.id)
-        }
-        return !match;
-      });
-      db.comments = db.comments.filter(comment => comment.author !== args.id);
-
-      return deletedUsers[0];
-    },
-    createPost(parent, args, { db }, info) {
-      const userExists = db.users.some(user => user.id === args.data.author);
-      if(!userExists) {
-        throw Error('User not found.');
-      }
-      const post = {
-        id: uuidv4(),
-        ...args.data
-      };
-
-      db.posts.push(post);
-
-      return post;
-    },
-    deletePost(parent, args, { db }, info) {
-      const postIndex = db.posts.findIndex(post => post.id === args.id);
-
-      if(postIndex === -1) {
-        throw new Error('Post not found.');
-      }
-      const deletedPosts = db.posts.splice(postIndex, 1);
-      db.comments = db.comments.filter(comment => comment.post !== args.id);
-      return deletedPosts[0];
-    },
-    createComment(parent, args, { db }, info) {
-      const postExists = db.posts.some(post => (post.id === args.data.post) && post.published);
-      const userExists = db.users.some(user => user.id === args.data.author);
-      if(!postExists) {
-        throw Error('Post not found or published');
-      }
-      if(!userExists) {
-        throw Error('User not found');
-      }
-      const comment = {
-        id: uuidv4(),
-        ...args.data
-      }
-      db.comments.push(comment);
-      return comment;
-    },
-    deleteComment(parent, args, { db }, info) {
-      const commentIndex = db.comments.findIndex(comment => comment.id === args.id);
-      if(commentIndex === -1) {
-        throw new Error("Comment not found");
-      }
-      const deletedComments = db.comments.splice(commentIndex, 1);
-
-      return deletedComments[0];
-    }
-  },
-  Post: {
-    author(parent, args, { db }, info) {
-      return db.users.find(user => user.id === parent.author);
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter(comment => comment.post=== parent.id);
-    }
-  },
-  Comment: {
-    author(parent, args, { db }, info) {
-      return db.users.find(user => user.id === parent.author);
-    },
-    post(parent, args, { db }, info) {
-      return db.posts.find(post => post.id === parent.post);
-    }
-  },
-  User: {
-    posts(parent, args, { db }, info) {
-      return db.posts.filter(post => post.author === parent.id);
-    },
-    comments(parent, args, { db }, info) {
-      return db.comments.filter(comment => comment.author === parent.id);
-    }
-  }
+  Query,
+  Mutation,
+  User,
+  Post,
+  Comment
 };
 
 const server = new GraphQLServer({
